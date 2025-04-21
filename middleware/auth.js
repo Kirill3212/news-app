27a0d801963bc225e55e341115101e4da31ef762
Kirill Terekhin <kirill.terekhin.98@bk.ru>
2025-04-21 22:10:33 +0300

    fix middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -17,7 +17,7 @@ const authMiddleware = async (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(decoded.id).select("-password");
+    const user = await User.findById(decoded.userId).select("-password");
     if (!user) {
       return res.status(401).json({ message: "Invalid token: user not found" });
     }
